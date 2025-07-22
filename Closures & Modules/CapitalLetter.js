841305bd4326bf4026ee0282624167e37c351e1a
Or Moscovitz <ormosco41@gmail.com>
2025-07-22 11:35:12 +0300

    managersongs

--- Closures & Modules/CapitalLetter.js
+++ Closures & Modules/CapitalLetter.js
@@ -20,3 +20,24 @@ const formatter = StringFormatter();
 
 console.log(formatter.capitalizeFirst("banana")); // Banana
 console.log(formatter.toSkewerCase("yellow fruit")); // yellow-fruit
+
+
+
+
+const SongsManager = function () {
+    const _songs = {}
+    const _youtubeBase = "https://www.youtube.com/watch?v="
+
+    //assumes the given URL is in the form of https://www.youtube.com/watch?v=kJQP7kiw5Fk
+    const _extractIdentifier = url => url.split("").splice(32).join("")
+    const _getUrl = identifier => _youtubeBase + identifier
+
+    const addSong = (name, url) => _songs[name] = _extractIdentifier(url)
+    const getSong = name => console.log(_getUrl(_songs[name]))
+
+    return {
+        addSong: addSong,
+        getSong: getSong
+    }
+}
+
