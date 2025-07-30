c34c5715b768ebf71cbb59f4c36641bf2e9a7f4f
Or Moscovitz <ormosco41@gmail.com>
2025-07-30 16:06:16 +0300

    quote

--- rupg/model.js
+++ rupg/model.js
@@ -1,17 +1,20 @@
 
 
 
-
-
-
-
-
-
+export class UserPage {
+    constructor(mainUser, quote, pokemon,about, friends) {
+        this.mainUser = mainUser;
+        this.quote = quote;
+        this.pokemon = pokemon;
+        this.about = about;
+        this.friends = friends;
+    }
+}
 
 
 
 export async function getMainUser() {
-    const response = await fetch('api');
+    const response = await fetch('https://randomuser.me/api/?results=1');
     const data = await response.json();
     const user = data.results[0];
     return {
@@ -20,7 +23,12 @@ export async function getMainUser() {
         City: user.location.city,
         State: user.location.state,
         Pictue: user.pictue.large
-
-
     };
+}
+
+
+export async function getQuote() {
+    const response = await fetch('https://api.kanye.rest');
+    const data = await response.json();
+    return data.quote;
 }
