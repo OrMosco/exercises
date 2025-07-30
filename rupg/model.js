e03000e4dc7648c75fbe2e054528c92e51e84e4f
Or Moscovitz <ormosco41@gmail.com>
2025-07-30 16:39:39 +0300

    kanyetext2

--- rupg/model.js
+++ rupg/model.js
@@ -11,25 +11,24 @@ export class UserPage {
     }
 }
 
-
-
 export async function getMainUser() {
     const response = await fetch('https://randomuser.me/api/?results=1');
     const data = await response.json();
     const user = data.results[0];
     return {
-        FirstName: user.name.first,
-        lastName: user.name.last,
-        City: user.location.city,
-        State: user.location.state,
-        Pictue: user.picture.large
-    };
-}
+    firstName: user.name.first,
+    lastName: user.name.last,
+    city: user.location.city,
+    state: user.location.state,
+    picture: user.picture.large
+};
 
+}
 
 export async function getQuote() {
     const response = await fetch('https://api.kanye.rest');
     const data = await response.json();
     return data.quote;
 }
+
 
