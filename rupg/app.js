88937759aa75f46cad4a1472f71d4faab9398d41
Or Moscovitz <ormosco41@gmail.com>
2025-07-30 17:03:52 +0300

    fixpoke

--- rupg/app.js
+++ rupg/app.js
@@ -1,5 +1,13 @@
 
 import { getMainUser,getQuote,getPokemon,getAbout,getFriends,UserPage } from "./model.js";
+import { renderUserPage } from "./view.js";
+
+document.getElementById("generate-btn").addEventListener("click", async () => {
+  const userPage = await getUserPage();
+  renderUserPage(userPage);
+});
+
+
 
 async function getUserPage() {
     const mainUser = await getMainUser();
